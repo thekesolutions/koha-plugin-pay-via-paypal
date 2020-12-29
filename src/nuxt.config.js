6e5e3d10ac0cd384b12c88b72aa820aff8bf3b64
Tomas Cohen Arazi <tomascohen@theke.io>
2020-12-29 07:59:54 -0300

    Issue #10: Get rid of the 'src - src' title
    This patch is far from ideal (i.e. this should be translatable) but
    showing 'src - src' makes no sense.
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
+    titleTemplate: 'Koha › Plugins › Pay via PayPal',
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
