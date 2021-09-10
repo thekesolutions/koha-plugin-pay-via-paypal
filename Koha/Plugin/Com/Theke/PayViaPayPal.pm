afe9dc00b31bafcb27854a6f6c9fea276b8d6993
Tomas Cohen Arazi <tomascohen@theke.io>
2021-09-10 11:42:03 -0300

    Explicitly import C4::Auth::get_template_and_user

--- Koha/Plugin/Com/Theke/PayViaPayPal.pm
+++ Koha/Plugin/Com/Theke/PayViaPayPal.pm
@@ -9,7 +9,7 @@ use base qw(Koha::Plugins::Base);
 ## We will also need to include any Koha libraries we want to access
 use C4::Context;
 use C4::Output;
-use C4::Auth;
+use C4::Auth qw(get_template_and_user);
 use C4::Languages;
 use Koha::Patron;
 use Koha::DateUtils;
