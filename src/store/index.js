3b62cad2ec50ee28cf8bebf69f180dc9aab34230
Tomas Cohen Arazi <tomascohen@theke.io>
2021-11-23 09:24:42 -0300

    Tidy files
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- src/store/index.js
+++ src/store/index.js
@@ -1,73 +1,73 @@
 import koha_api from '~/koha-api.json'
 
 export const state = () => ({
-  general: {},
-  libConfs: [],
-  clean: true,
-  snackbar: false
+    general: {},
+    libConfs: [],
+    clean: true,
+    snackbar: false
 })
 
 export const actions = {
-  async get_configs ({ commit, dispatch }) {
-    dispatch('schema/get_schema')
-    dispatch('libraries/get_libraries')
+    async get_configs({ commit, dispatch }) {
+        dispatch('schema/get_schema')
+        dispatch('libraries/get_libraries')
 
-    try {
-      const data = await this.$axios.$get(koha_api.path +'/configs')
-      
-      if (!data.general) {
-        data.general = { PayPalSandboxMode: false, useBaseURL: true  }
-      }
-      if (!data.libraries || !data.libraries.length) {
-        // throw new Error('Could not get configuration list')
-        data.libraries = [{ active: true, library_id: null }]
-      }
+        try {
+            const data = await this.$axios.$get(koha_api.path + '/configs')
 
-      commit('general_confs', data.general)
-      commit('library_confs', data.libraries)
-    } catch (e) {
-      console.log(e)
-      commit('snackbar', { type: 'error', message: this.app.i18n.t('connection.noConfigs') })
+            if (!data.general) {
+                data.general = { PayPalSandboxMode: false, useBaseURL: true }
+            }
+            if (!data.libraries || !data.libraries.length) {
+                // throw new Error('Could not get configuration list')
+                data.libraries = [{ active: true, library_id: null }]
+            }
+
+            commit('general_confs', data.general)
+            commit('library_confs', data.libraries)
+        } catch (e) {
+            console.log(e)
+            commit('snackbar', { type: 'error', message: this.app.i18n.t('connection.noConfigs') })
+        }
+    },
+    async save({ commit, state }) {
+        if (state.clean) {
+            return commit('snackbar', { type: 'info', message: this.app.i18n.t('noSave') })
+        }
+        if (state.schema.schema.properties.generalOptions) {
+            await this.$axios.$post(koha_api.path + '/configs/general', state.general)
+        }
+        if (state.schema.schema.properties.perLibraryOptions) {
+            //Koha's openapi version does not allow nullable properties in objects
+            //this hack is to filter null columns
+            const confs = [];
+            state.libConfs.forEach(conf => {
+                const tmpcnf = {}
+                Object.keys(conf)
+                    .filter(key => conf[key] !== null)
+                    .forEach(key => tmpcnf[key] = conf[key])
+                confs.push(tmpcnf)
+            })
+            await this.$axios.$post(koha_api.path + '/configs/library', confs)
+        }
+        commit('clean')
+        commit('snackbar', { type: 'success', message: this.app.i18n.t('saved') })
     }
-  },
-  async save ({ commit, state }) {
-    if (state.clean) {
-      return commit('snackbar', { type: 'info', message: this.app.i18n.t('noSave') })
-    }
-    if (state.schema.schema.properties.generalOptions) {
-      await this.$axios.$post(koha_api.path +'/configs/general', state.general)
-    }
-    if (state.schema.schema.properties.perLibraryOptions) {
-      //Koha's openapi version does not allow nullable properties in objects
-      //this hack is to filter null columns
-      const confs = [];
-      state.libConfs.forEach(conf => {
-        const tmpcnf = {}
-        Object.keys(conf)
-          .filter(key => conf[key] !== null)
-          .forEach(key => tmpcnf[key] = conf[key])
-        confs.push(tmpcnf)
-      })
-      await this.$axios.$post(koha_api.path +'/configs/library', confs)
-    }
-    commit('clean')
-    commit('snackbar', { type: 'success', message: this.app.i18n.t('saved') })
-  }
 }
 
 export const mutations = {
-  general_confs (state, data) {
-    state.clean = false
-    state.general = data
-  },
-  library_confs (state, data) {
-    state.clean = false
-    state.libConfs = data
-  },
-  snackbar (state, data) {
-    state.snackbar = data
-  },
-  clean (state) {
-    state.clean = true
-  }
+    general_confs(state, data) {
+        state.clean = false
+        state.general = data
+    },
+    library_confs(state, data) {
+        state.clean = false
+        state.libConfs = data
+    },
+    snackbar(state, data) {
+        state.snackbar = data
+    },
+    clean(state) {
+        state.clean = true
+    }
 }
--- src/store/libraries.js
+++ src/store/libraries.js
@@ -1,17 +1,17 @@
 export const state = () => ({
-  list: []
+    list: []
 })
 
 export const actions = {
-  async get_libraries ({ commit }) {
-    const data = await this.$axios.$get('/api/v1/libraries?_per_page=-1')
-    data.unshift({ library_id: null, name: 'All libraries' })
-    commit('set_libraries', data)
-  }
+    async get_libraries({ commit }) {
+        const data = await this.$axios.$get('/api/v1/libraries?_per_page=-1')
+        data.unshift({ library_id: null, name: 'All libraries' })
+        commit('set_libraries', data)
+    }
 }
 
 export const mutations = {
-  set_libraries (state, data) {
-    state.list = data
-  }
+    set_libraries(state, data) {
+        state.list = data
+    }
 }
